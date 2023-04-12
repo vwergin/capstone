d5110b1b45d05489afc13f0e73089dd199f825dc
vwergin <v.g.wergin@wustl.edu>
2023-04-12 15:19:57 -0500

    push

--- canny.py
+++ canny.py
@@ -6,7 +6,7 @@ img = cv2.imread("first_road5.jpg")
 hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 print(img.shape)
 img2 = img[400:720, 0:1280]
-mask = cv2.inRange(img2, (40,10,10), (80,100,100))
+mask = cv2.inRange(img2, (40,10,10), (80,95,95))
 t_lower = 150
 t_upper = 200
 edge = cv2.Canny(mask, t_lower, t_upper, L2gradient = True)
---
+++ track_black.py
@@ -0,0 +1,25 @@
+import cv2
+import time
+
+img = cv2.imread("first_road6.jpg")
+
+hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+#print(img.shape)
+img2 = img[400:720, 0:1280]
+print(img2.shape)
+mask = cv2.inRange(img2, (40,10,10), (80,95,95))
+row = 0
+for i in range(180,215):
+    white = 0
+    for j in range(550, 750):
+#        print(mask[i,j])
+        if mask[i,j] == 255:
+            white = 1
+    if white == 0:
+        row = i
+        break
+print("row",row)
+cv2.imshow('original', mask)
+cv2.waitKey(5000)
+#time.sleep(10)
+cv2.destroyAllWindows()
